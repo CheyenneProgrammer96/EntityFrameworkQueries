b9dab808266da77f3885c3e3f61a71c375d5760e
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-02-07 16:45:49 -0800

    Added query to demonstrate WHERE and ORDER BY

--- EntityFrameworkQueries/Form1.Designer.cs
+++ EntityFrameworkQueries/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.btnSelectAllVendors = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnAllCaliVendors = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -43,18 +43,19 @@
             this.btnSelectAllVendors.UseVisualStyleBackColor = true;
             this.btnSelectAllVendors.Click += new System.EventHandler(this.btnSelectAllVendors_Click);
             // 
-            // button2
+            // btnAllCaliVendors
             // 
-            this.button2.Location = new System.Drawing.Point(121, 170);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 46);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAllCaliVendors.Location = new System.Drawing.Point(121, 170);
+            this.btnAllCaliVendors.Name = "btnAllCaliVendors";
+            this.btnAllCaliVendors.Size = new System.Drawing.Size(891, 70);
+            this.btnAllCaliVendors.TabIndex = 1;
+            this.btnAllCaliVendors.Text = "SELECT * FROM Vendors WHERE VendorState = \'CA\' ORDER BY VendorName ASC";
+            this.btnAllCaliVendors.UseVisualStyleBackColor = true;
+            this.btnAllCaliVendors.Click += new System.EventHandler(this.btnAllCaliVendors_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(121, 273);
+            this.button3.Location = new System.Drawing.Point(121, 317);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(150, 46);
             this.button3.TabIndex = 2;
@@ -67,7 +68,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1458, 899);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAllCaliVendors);
             this.Controls.Add(this.btnSelectAllVendors);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -78,7 +79,7 @@
         #endregion
 
         private Button btnSelectAllVendors;
-        private Button button2;
+        private Button btnAllCaliVendors;
         private Button button3;
     }
 }
--- EntityFrameworkQueries/Form1.cs
+++ EntityFrameworkQueries/Form1.cs
@@ -21,5 +21,22 @@ namespace EntityFrameworkQueries
             List<Vendor> vendorList2 = (from v in dbContext.Vendors
                                        select v).ToList();
         }
+
+        private void btnAllCaliVendors_Click(object sender, EventArgs e)
+        {
+            using ApContext dbContext = new();
+
+            // LINQ (Langauge Integrated Query) method syntax
+            List<Vendor> vendorList = dbContext.Vendors
+                                        .Where(v => v.VendorState == "CA")
+                                        .OrderBy(v => v.VendorName)
+                                        .ToList();
+
+            // LINQ query syntax
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                       where v.VendorState == "CA"
+                                       orderby v.VendorName
+                                       select v).ToList();
+        }
     }
 }
